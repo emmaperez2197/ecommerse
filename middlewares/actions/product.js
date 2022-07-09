251171dcf39e50f845fb4c0168bebd63acfea4f6
marcosgabriel2297 <mvera@moodtechnology.com.ar>
2022-07-09 14:08:20 -0300

    se agregaron nuevas validaciones

--- middlewares/actions/product.js
+++ middlewares/actions/product.js
@@ -2,7 +2,7 @@ const schema = require('../schemas/product');
 
 const validateCreate =  (req, res, next)=>{
 
-    const {error, value } = schema.create.validate(req.body);
+    const { error } = schema.create.validate(req.body);
     error ? res.status(422).json({message:'datos incorrectos'}) : next()
 }
 
