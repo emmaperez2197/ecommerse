4271ff2a115051842173f68b17ed800c1c70ca7e
marcosgabriel2297 <mvera@moodtechnology.com.ar>
2022-07-15 17:04:49 -0300

    se modifico el .env

--- controllers/products/findByid.js
+++ controllers/products/findByid.js
@@ -1,6 +1,21 @@
 const Product = require('../../models/products/Product');
 
-const findByIdProducts  = ( req, res) =>{
-    console.log('hola');
+const findByIdProduct  = async(req, res) => {
+
+
+    const {id} = req.params;
+    
+    try {
+        const findByidProduct =  await Product.findById(id);
+        if (!findByidProduct) {
+            return  res.status(402).json({ message:  `no se encontro el producto con el id: ${req.params.id} `});
+        }
+
+        return res.status(200).json({findByidProduct})
+    } catch (error) {
+        return  res.status(500).json({ message: error.toString() })
+    }
 }
- module.exports = {findByIdProducts}
+ module.exports = {
+    findByIdProduct
+}
