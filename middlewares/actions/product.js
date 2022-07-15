eb5153ee135c61c2049c6a7524205f9c2868749a
marcosgabriel2297 <mvera@moodtechnology.com.ar>
2022-07-15 14:59:30 -0300

    modificacion

--- middlewares/actions/product.js
+++ middlewares/actions/product.js
@@ -1,9 +1,9 @@
 const schema = require('../schemas/product');
 
-const validateCreate = (req, res, next) => {
+const validateCreate =  (req, res, next)=>{
 
-	const { error } = schema.create.validate(req.body);
-	error ? res.status(422).json({ message: 'datos incorrectos' }) : next();
-};
+    const { error } = schema.create.validate(req.body);
+    error ? res.status(422).json({message:'datos incorrectos'}) : next()
+}
 
-module.exports = validateCreate;
+module.exports = validateCreate
