01e8a6d97b6a42d6504e6bb9cae4fc0babd3431c
marcosgabriel2297 <mvera@moodtechnology.com.ar>
2022-07-15 18:13:49 -0300

    se creo la carpeta services y las funciones para usar jsonwebtoken

---
+++ services/token.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const singOptions = { expiresIn: "8h", algorithm: "HS256" };
+
+const createToken = (paylaoad) =>  jwt.sign(paylaoad, 'emmA777@RicO$', singOptions)
+
+const decodeToken = (token) => {
+    
+
+    const [, JWT] = token.split(' ');
+    const validToken= jwt.verify(JWT, 'emmA777@RicO$' );
+    return validToken
+}
+
+
+module.exports = {
+    createToken,
+    decodeToken
+}  
+
