0db9c31f3bd1e368478774b58747c6d6ed7297f8
marcosgabriel2297 <mvera@moodtechnology.com.ar>
2022-07-19 22:49:03 -0300

    se termino el EP para loguear al usuario

--- controllers/login/login.js
+++ controllers/login/login.js
@@ -1,13 +1,32 @@
 const User = require('../../models/user/User');
 
+const {anHash}= require('../../utils/bcrypt')
+const {createToken} = require('../../services/token')
 
-// const User = require('../../models/user/User');
+const login = async (req, res) => {
 
+    const { email, password} = req.body;
 
-const login = async(req, res) => {
+    try {
+        const loginUser =  await User.findOne( { email: email}, {password: 1} );
+        const validatePassword = anHash(password, loginUser.password)
 
-    console.log('hola');
+        console.log(loginUser._id);
+        const JWTObject = { 
+            id: login._id,
+            email
+        }
 
+        const JWT = createToken(JWTObject)
+
+        if (!validatePassword) {
+            return res.status(200).json({message: 'email o contraseña incorrectos, intentelo de nuevo!'})
+        }
+        
+        return res.status(200).json({ message: ' Bienvenido!', JWT })
+    } catch (error) {
+        return res.status(500).json({ message: error.toString() })
+    }
 }
 
 module.exports = {
--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,8 @@ const router = Router();
 
 const { registrar } = require('../controllers/login/registrar');
 const { login } = require('../controllers/login/login') 
-const { validateRegistro } = require('../middlewares/actions/registro');
 
+const { validateRegistro } = require('../middlewares/actions/registro');
 
 router.post('/registrar', validateRegistro, registrar);
 router.post('/login', login);
