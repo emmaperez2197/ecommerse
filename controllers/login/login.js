cb302e7552331dc00fc2edca29433a80e7d44982
marcosgabriel2297 <mvera@moodtechnology.com.ar>
2022-07-19 21:24:45 -0300

    se creo el archivo login en las rutas

---
+++ controllers/login/login.js
@@ -0,0 +1,15 @@
+const User = require('../../models/user/User');
+
+
+// const User = require('../../models/user/User');
+
+
+const login = async(req, res) => {
+
+    console.log('hola');
+
+}
+
+module.exports = {
+    login
+}
--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,11 @@ const { Router } = require('express');
 const router = Router();
 
 const { registrar } = require('../controllers/login/registrar');
-
+const { login } = require('../controllers/login/login') 
 const { validateRegistro } = require('../middlewares/actions/registro');
 
-router.post('/create', validateRegistro, registrar);
+
+router.post('/registrar', validateRegistro, registrar);
+router.post('/login', login);
 
 module.exports = router;
